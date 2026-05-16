dd6c58894528156c3ee0da30ae29808ca2a160c9
Tulipurple <164765964+Tulipurple@users.noreply.github.com>
2026-05-16 16:58:23 +0900

    feat: 기본 세팅

---
+++ NTeam-BE/src/main/java/com/example/nteambe/NTeamBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.nteambe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NTeamBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NTeamBeApplication.class, args);
+    }
+
+}
---
+++ NTeam-BE/src/test/java/com/example/nteambe/NTeamBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nteambe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NTeamBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
