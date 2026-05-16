e41598498209ea8a52dd91c09cf569dd18f9c504
Tulipurple <164765964+Tulipurple@users.noreply.github.com>
2026-05-16 21:59:33 +0900

    feat: 저장된 스팟 API

--- NTeam-BE/src/main/java/com/example/nteambe/domain/spot/exception/code/SpotErrorCode.java
+++ NTeam-BE/src/main/java/com/example/nteambe/domain/spot/exception/code/SpotErrorCode.java
@@ -1,4 +1,17 @@
 package com.example.nteambe.domain.spot.exception.code;
 
-public enum SpotErrorCode {
+import com.example.nteambe.global.apiPayload.code.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum SpotErrorCode implements BaseErrorCode {
+    SPOT_NOT_FOUND(HttpStatus.NOT_FOUND, "SPOT404_1", "존재하지 않는 스팟은 저장할 수 없습니다."),
+    SAVED_SPOT_ALREADY_SAVED(HttpStatus.CONFLICT, "SAVED_SPOT409_1", "이미 저장된 스팟입니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
